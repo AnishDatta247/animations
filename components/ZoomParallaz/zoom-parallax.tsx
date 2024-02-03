50613a90b7d43c357ea3deaa9f11f14a3dfbd934
Anish Datta <anishd3139@gmail.com>
2024-02-03 15:33:26 +0530

    first commit

---
+++ components/ZoomParallaz/zoom-parallax.tsx
@@ -0,0 +1,86 @@
+import Image, { StaticImageData } from "next/image";
+import { useScroll, useTransform, motion } from "framer-motion";
+import { useRef } from "react";
+import styles from "./styles.module.scss";
+
+interface ZoomParallaxProps {
+  data: {
+    src: StaticImageData;
+  }[];
+  title: string;
+  fontSize: string;
+}
+
+export const ZoomParallax = ({ data, title, fontSize }: ZoomParallaxProps) => {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  console.log(title, fontSize, `text-[${fontSize}] ${styles.underlayText}`);
+
+  const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+  const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
+  const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8]);
+  const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9]);
+
+  const pictures = [
+    {
+      src: data[0].src,
+      scale: scale4,
+    },
+    {
+      src: data[1].src,
+      scale: scale5,
+    },
+    {
+      src: data[2].src,
+      scale: scale6,
+    },
+    {
+      src: data[3].src,
+      scale: scale5,
+    },
+    {
+      src: data[4].src,
+      scale: scale6,
+    },
+    {
+      src: data[5].src,
+      scale: scale8,
+    },
+    {
+      src: data[6].src,
+      scale: scale9,
+    },
+  ];
+
+  return (
+    <div ref={container} className={styles.container}>
+      <div className={styles.sticky}>
+        {fontSize && (
+          <span className={`text-\[${fontSize}\] ${styles.underlayText}`}>
+            {title}
+          </span>
+        )}
+        {fontSize && (
+          <span className={`text-\[${fontSize}\] ${styles.overlayText}`}>
+            {title}
+          </span>
+        )}
+        {pictures.map(({ src, scale }, index) => {
+          return (
+            <motion.div key={index} style={{ scale }} className={styles.el}>
+              <div className={styles.imageContainer}>
+                <Image src={src} fill alt="image" placeholder="blur" />
+              </div>
+            </motion.div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
---
+++ data/zoom-parallax.ts
@@ -0,0 +1,31 @@
+import image1 from "../public/ZP1.jpeg";
+import image2 from "../public/ZP2.jpeg";
+import image3 from "../public/ZP3.jpeg";
+import image4 from "../public/ZP4.jpeg";
+import image5 from "../public/ZP5.jpeg";
+import image6 from "../public/ZP1.jpeg";
+import image7 from "../public/ZP2.jpeg";
+
+export const ZoomParallaxData = [
+  {
+    src: image1,
+  },
+  {
+    src: image2,
+  },
+  {
+    src: image3,
+  },
+  {
+    src: image4,
+  },
+  {
+    src: image5,
+  },
+  {
+    src: image1,
+  },
+  {
+    src: image2,
+  },
+];
